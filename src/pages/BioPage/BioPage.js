6e32a8fe32ec909495edd6b5448ce8cfeb4f7642
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-04 11:02:56 -0400

    added paralax scrolling

--- src/pages/BioPage/BioPage.js
+++ src/pages/BioPage/BioPage.js
@@ -10,7 +10,12 @@ const BioPage = () => {
                 <div className={styles.bioPage}>
                     <div className={styles.bioSection}>
                         <img className={styles.bioImage} src="https://i.imgur.com/KnCMI31.jpg" alt="" />
-                        <p className={styles.bioText}>Hello there! My name is Anthony Rondina and I am a software developer commited to providing products of high quality. I love starting a project with nothing and seeing it through until completion. If after you view my work and want to connect, I look forward to speaking with you!</p>
+                        <p className={styles.bioText}>Hello there! My name is Anthony Rondina and I am a software developer commited to providing products of high quality. I love starting a project from nothing and seeing it through until completion. If after you view my work and want to connect, I look forward to speaking with you!</p>
+                    </div>
+                    <div class={styles.para}>
+                        <div class={styles.parainner}>
+
+                        </div>
                     </div>
                     <div>
                         <h2>Projects</h2>
@@ -22,6 +27,12 @@ const BioPage = () => {
                         <Link to="/choose-your-own-adventure"><img className={styles.projectImage} src="https://i.imgur.com/ewp4GtW.jpg" alt="" /></Link>
                         <Link to="/amazon"><img className={styles.projectImage} src="https://i.imgur.com/56H6x0p.png" alt="" /></Link>
                         <Link to="/atelier-collab"><img className={styles.projectImage} src="https://i.imgur.com/aPydPtB.png" alt="" /></Link>
+                    </div>
+                    <hr className={styles.line4} />
+                    <div class={styles.para2}>
+                        <div class={styles.parainner}>
+
+                        </div>
                     </div>
                     <div>
                         <h2>Contact</h2>
