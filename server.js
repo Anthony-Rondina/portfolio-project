f1ee7ea9d736eeb7ba529aae1b079018f3f6bab7
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-12 15:10:33 -0400

    updated responsivness to 600px

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cors = require('cors')
 const app = express();
-
-
-require('dotenv').config();
-require('./config/db');
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,15 +18,15 @@ app.use(fileUpload({
 
 
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 /* Configure to use port 3001 instead of 3000 during
  development to avoid collision with React's dev server  */
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Express app running on port ${port}`)
 });
